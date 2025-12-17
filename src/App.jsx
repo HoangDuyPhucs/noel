15e5fe054d9549e8a9a549359f7ce4866674a975
Hoang Phuc <phuchd3112@gmail.com>
2025-12-18 01:00:23 +0700

    Update UI / new feature2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import HelloWorld from "./HelloWorld";
+import HelloWorld2 from "./HelloWorld2";
 
 function App() {
-  return <HelloWorld />;
+  return (<>
+  <HelloWorld />
+    <HelloWorld2 />
+  </>
+  );
 }
 
 export default App;
