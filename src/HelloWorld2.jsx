2f9acf803e198eacda9125e4805b2091a355a8fb
Hoang Phuc <phuchd3112@gmail.com>
2025-12-18 00:54:24 +0700

    Update UI / new feature

---
+++ src/HelloWorld2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      Hello World 22222
+    </div>
+  );
+}
